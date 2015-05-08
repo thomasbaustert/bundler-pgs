cf0291f3d631d8c10a6bd47777d34ce844d6ddcd
Thomas Baustert <business@thomasbaustert.de>
2015-05-08 11:05:23 +0200

    remove warning about unsupported Bundler version

--- lib/bundler-pgs.rb
+++ lib/bundler-pgs.rb
@@ -1,11 +1,4 @@
 require 'bundler/version'
-
-if Bundler::VERSION > "1.7.4"
-  puts "\nWarning: You are using bundler #{Bundler::VERSION} and bundler-pgs patch might not work for this version."
-  puts "Make sure your Gemfile.lock does not contain your credentials after running bundle-pgs!"
-  puts ""
-end
-
 require "bundler-pgs/version"
 require "bundler-pgs/bundler_patch"
 
